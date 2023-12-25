3f52f32d6f0afb7d8d8ffb9d58290cc2df013ed3
Mohamed Khettab <93957717+mohamed-khettab@users.noreply.github.com>
2023-12-25 15:43:49 -0500

    Update Footer.js

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="bg-primary py-6">
       <div className="max-container flex justify-center items-center text-white">
-        <p className="text-sm lg:text-xl font-body">Darija Dictionary</p>
+        <div className="flex items-center ml-4">
+          <a href="https://github.com/mohamed-khettab/darija-dictionary" target="_blank" rel="noopener noreferrer" className="text-white">
+            <FaGithub className="mr-2" />
+          </a>
+          <a href="https://github.com/mohamed-khettab/darija-dictionary" target="_blank" rel="noopener noreferrer" className="text-sm lg:text-xl font-body">Made by Mohamed Khettab</a>
+        </div>
       </div>
     </footer>
   );
