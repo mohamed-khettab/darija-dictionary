159727acb7e3f37239b47cea4a450b3441398b24
Mohamed Khettab <93957717+mohamed-khettab@users.noreply.github.com>
2023-12-25 15:22:38 -0500

    Update fetchData.js

--- frontend/src/utils/fetchData.js
+++ frontend/src/utils/fetchData.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://darija-dictionary-api.onrender.com/api',
 });
 
 const fetchWord = async (word, setWordData) => {
